780013e28b3a30a20c839f4e362b60290a647ee4
Jvictor3141 <jvcajado2.0@gmail.com>
2026-03-14 20:54:08 -0300

    corrigindo codigo

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
 
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function() {
   const endpoint = `https://api.github.com/users/Jvictor3141`;
-  const name = $('#name');
-  const user = $('#profile-username');
-  const seguidores = $('#seguidores');
-  const seguindo = $('#seguindo');
-  const avaUrl = $('#profile-avatar');
-  const repos = $('#repositorios');
-  const link = $('#profile-link');
+  const name = document.getElementById('name');
+  const user = document.getElementById('profile-username');
+  const seguidores = document.getElementById('seguidores');
+  const seguindo = document.getElementById('seguindo');
+  const avaUrl = document.getElementById('profile-avatar');
+  const repos = document.getElementById('repositorios');
+  const link = document.getElementById('profile-link');
   
-  
-  $.getJSON(endpoint, function(json) {
-    name.text(json.name);
-    user.text(json.login);
-    seguidores.text(json.followers);
-    seguindo.text(json.following);
-    avaUrl.attr('src', json.avatar_url);
-    repos.text(json.public_repos);
-    link.attr('href', json.html_url);
-  });
+  fetch(endpoint)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      name.textContent = json.name;
+      user.textContent = json.login;
+      seguidores.textContent = json.followers;
+      seguindo.textContent = json.following;
+      avaUrl.src = json.avatar_url;
+      repos.textContent = json.public_repos;
+      link.href = json.html_url;
+    })
+    .catch(function(erro) {
+      alert('Ocorreu um erro ao buscar o endereço. Por favor, tente novamente.');
+      console.error('Erro:', erro);
+    });
 });
