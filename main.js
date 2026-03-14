07194c9e1e899cccfa7e1f73ccf9bf5942df294c
Jvictor3141 <jvcajado2.0@gmail.com>
2026-03-14 20:37:38 -0300

    atualizando codigo

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-  const endpoint = `https://api.github.com/users/Jvictor3141`
-  const name = document.querySelector('#name');
-  const user = document.querySelector('#profile-username');
-  const seguidores = document.querySelector('#seguidores');
-  const seguindo = document.querySelector('#seguindo');
-  const avaUrl = document.querySelector('#profile-avatar');
-  const repos = document.querySelector('#repositorios');
-  const link = document.querySelector('#profile-link');
+$(document).ready(function() {
+  const endpoint = `https://api.github.com/users/Jvictor3141`;
+  const name = $('#name');
+  const user = $('#profile-username');
+  const seguidores = $('#seguidores');
+  const seguindo = $('#seguindo');
+  const avaUrl = $('#profile-avatar');
+  const repos = $('#repositorios');
+  const link = $('#profile-link');
   
   
-  fetch(endpoint)
-    .then(function(resposta) {
-      return resposta.json();
-    })
-    .then(function(json) {
-      name.innerText = json.name;
-      user.innerText = json.login;
-      seguidores.innerText = json.followers;
-      seguindo.innerText = json.following;
-      avaUrl.src = json.avatar_url;
-      repos.innerText = json.public_repos;
-      link.href = json.html_url;
-    })
-
-})
+  $.getJSON(endpoint, function(json) {
+    name.text(json.name);
+    user.text(json.login);
+    seguidores.text(json.followers);
+    seguindo.text(json.following);
+    avaUrl.attr('src', json.avatar_url);
+    repos.text(json.public_repos);
+    link.attr('href', json.html_url);
+  });
+});
